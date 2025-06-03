2841da04be6326069c7ef9ca9af1c7604737ec65
Ayoub Fateh Allah Larbaoui <ayoublarbaoui004@gmail.com>
2025-06-03 04:39:41 +0100

    refactor: enhance AppTypography with additional text styles for headings and body

--- lib/config/theme/typoghrapy_manager.dart
+++ lib/config/theme/typoghrapy_manager.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 class AppTypography {
   //Fonts
   static const String appFont = "Satoshi";
-
+  //-------------------------------
   //Font Weights
   static const FontWeight appFontBold = FontWeight.bold;
   static const FontWeight appFontSemiBold = FontWeight.w600;
   static const FontWeight appFontMedium = FontWeight.w500;
   static const FontWeight appFontRegular = FontWeight.w400;
-
+  //--------------------------------
   //Font Sizes
+
   // Headings Text sizes
   static const double headLine1 = 28.0;
   static const double headLine2 = 20.0;
@@ -24,4 +25,87 @@ class AppTypography {
   static const double body3 = 14.0;
   static const double bodySmall = 13.0;
   static const double bodyXSmall = 12.0;
+  //-------------------------------------
+  // Headings Text Styles
+  static const TextStyle headLine1Style = TextStyle(
+    fontFamily: appFont,
+    fontSize: headLine1,
+    fontWeight: appFontBold,
+  );
+  static const TextStyle headLine2Style = TextStyle(
+    fontFamily: appFont,
+    fontSize: headLine2,
+    fontWeight: appFontBold,
+  );
+  static const TextStyle headLine3SemiBoldStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: headLine3,
+    fontWeight: appFontSemiBold,
+  );
+  static const TextStyle headLine3MediumStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: headLine3,
+    fontWeight: appFontMedium,
+  );
+  static const TextStyle headLine4SemiBoldStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: headLine4,
+    fontWeight: appFontSemiBold,
+  );
+  static const TextStyle headLine4MediumStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: headLine4,
+    fontWeight: appFontMedium,
+  );
+  static const TextStyle headLine5SemiBoldStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: headLine5,
+    fontWeight: appFontSemiBold,
+  );
+  static const TextStyle headLine5MediumStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: headLine5,
+    fontWeight: appFontMedium,
+  );
+  // Bodys Text Styles
+  static const TextStyle body1MediumStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: body1,
+    fontWeight: appFontMedium,
+  );
+  static const TextStyle body1RegularStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: body1,
+    fontWeight: appFontRegular,
+  );
+  static const TextStyle body2MediumStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: body2,
+    fontWeight: appFontMedium,
+  );
+  static const TextStyle body2RegularStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: body2,
+    fontWeight: appFontRegular,
+  );
+  static const TextStyle body3MediumStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: body3,
+    fontWeight: appFontMedium,
+  );
+  static const TextStyle body3RegularStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: body3,
+    fontWeight: appFontRegular,
+  );
+  static const TextStyle bodySmallStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: bodySmall,
+    fontWeight: appFontRegular,
+  );
+  static const TextStyle bodyXSmallStyle = TextStyle(
+    fontFamily: appFont,
+    fontSize: bodyXSmall,
+    fontWeight: appFontRegular,
+  );
 }
