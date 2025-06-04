3669b00ebd985b65f3dfeac83e340c5c6e824683
Ayoub Fateh Allah Larbaoui <ayoublarbaoui004@gmail.com>
2025-06-04 23:10:20 +0100

    feat: add CheckPhoneScreen, CheckPhoneHeaderSection, and CheckPhoneOtpInputSection components for phone verification flow

---
+++ lib/features/check_phone/widgets/check_phone_otp_input_section.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:hader_pharm_mobile/config/theme/colors_manager.dart';
+import 'package:hader_pharm_mobile/config/theme/typoghrapy_manager.dart';
+import 'package:hader_pharm_mobile/utils/assets_strings.dart';
+
+import 'package:hader_pharm_mobile/utils/constants.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../utils/enums.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+
+import '../../common/buttons/solid/primary_text_button.dart';
+
+class CheckPhoneOtpInputSection extends StatelessWidget {
+  late List<TextEditingController?> controls;
+  int numberOfFields = 5;
+  bool clearText = false;
+  CheckPhoneOtpInputSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        OtpTextField(
+          numberOfFields: 5,
+          borderColor: StrokeColors.normal.color,
+          focusedBorderColor: StrokeColors.focused.color,
+          clearText: clearText,
+          showFieldAsBox: true,
+          fieldHeight: 52.8,
+          fieldWidth: 52.8,
+          cursorColor: AppColors.accent1Shade1,
+          alignment: Alignment.center,
+
+          textStyle: AppTypography.body1MediumStyle.copyWith(fontSize: AppTypography.headLine4),
+          onCodeChanged: (String value) {
+            //Handle each value
+          },
+          handleControllers: (controllers) {
+            //get all textFields controller, if needed
+            controls = controllers;
+          },
+          onSubmit: (String verificationCode) {}, // end onSubmit
+        ),
+        Gap(AppSizesManager.s12),
+        Padding(
+          padding: const EdgeInsets.all(AppSizesManager.p12),
+          child: Text(
+            "Resend! (5s)",
+            style: AppTypography.body3RegularStyle.copyWith(
+                color: TextColors.secondary.color,
+                decoration: TextDecoration.underline,
+                decorationColor: TextColors.secondary.color),
+          ),
+        ),
+        Gap(AppSizesManager.s24),
+        PrimaryTextButton(
+          label: "Verify",
+          onTap: () {},
+          color: AppColors.accent1Shade1,
+        ),
+        Gap(AppSizesManager.s12),
+        PrimaryTextButton(
+          label: "Change Phone Number",
+          onTap: () {},
+          labelColor: AppColors.accent1Shade1,
+        ),
+      ],
+    );
+  }
+}
