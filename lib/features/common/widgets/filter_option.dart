6959acf5ec2c87dba7866c6d2bbd636c8236c8a1
Ayoub Fateh Allah Larbaoui <ayoublarbaoui004@gmail.com>
2025-06-10 11:18:02 +0200

    feat: add FilterOptionWidget and FilterOptionValueWidget components

---
+++ lib/features/common/widgets/filter_option.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:hader_pharm_mobile/config/theme/typoghrapy_manager.dart';
+import 'package:hader_pharm_mobile/features/common/buttons/solid/primary_icon_button.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../config/theme/colors_manager.dart';
+
+class FilterOptionWidget extends StatelessWidget {
+  final String title;
+  final String subTitle;
+
+  const FilterOptionWidget({super.key, this.title = 'Default', this.subTitle = 'Default'});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Text(
+                title,
+                style: AppTypography.body3MediumStyle,
+              ),
+              Text(
+                subTitle,
+                style: AppTypography.bodySmallStyle.copyWith(color: TextColors.ternary.color),
+              ),
+            ],
+          ),
+          Spacer(),
+          PrimaryIconButton(
+            icon: Icon(
+              Iconsax.arrow_right_3,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/common/widgets/filter_option_value.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:hader_pharm_mobile/config/theme/typoghrapy_manager.dart';
+
+import '../../../config/theme/colors_manager.dart';
+
+class FilterOptionValueWidget extends StatelessWidget {
+  final String title;
+  final bool isSelected;
+  const FilterOptionValueWidget({super.key, this.title = 'Default', this.isSelected = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Text(
+            title,
+            style: AppTypography.body3MediumStyle,
+          ),
+          Spacer(),
+          Container(
+            height: 20,
+            width: 20,
+            decoration: BoxDecoration(
+                border: Border.all(color: isSelected ? AppColors.accent1Shade1 : StrokeColors.normal.color, width: 1),
+                shape: BoxShape.circle),
+            child: Center(
+              child: Container(
+                  height: 12,
+                  width: 12,
+                  decoration: BoxDecoration(
+                      color: isSelected ? AppColors.accent1Shade1 : Colors.transparent, shape: BoxShape.circle)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
