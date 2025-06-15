68d4108bff64dab907bad321b924bcbd974cb276
Ayoub Fateh Allah Larbaoui <ayoublarbaoui004@gmail.com>
2025-06-15 23:26:04 +0100

    chore (/repositories/): remove unused repository files

--- lib/repositories/remote/payment_method/payment_method_repository.dart
+++
@@ -1,7 +0,0 @@
-//import '../../../model/person.dart';
-
-import '../../../models/payment_method.dart';
-
-abstract class IPaymentMethodRepository {
-  Future<List<PaymentMethod>> getPaymentMethods();
-}
--- lib/repositories/remote/promotion/promotion_repository.dart
+++
@@ -1,8 +0,0 @@
-//import '../../../model/person.dart';
-
-
-import '../../../models/promotion.dart';
-
-abstract class IPromotionRepository {
-  Future<List<PromotionModel>> getPromotions();
-}
