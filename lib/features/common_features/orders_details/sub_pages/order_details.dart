abc61983f9b4cd96e24dcebc8546d7c4d713d539
ayoub larbaoui <ayoublarbaoui004@gmail.com>
2025-11-15 08:38:56 -0800

    style (order-details): enhance order details page with new OrderInfosSection and improved its components ui ( part 1 ) .

--- lib/features/common_features/orders_details/sub_pages/order_details.dart
+++ lib/features/common_features/orders_details/sub_pages/order_details.dart
@@ -3,11 +3,15 @@ import 'package:flutter_bloc/flutter_bloc.dart' show BlocBuilder, ReadContext;
 import 'package:hader_pharm_mobile/config/routes/routing_manager.dart';
 import 'package:hader_pharm_mobile/config/theme/colors_manager.dart';
 import 'package:hader_pharm_mobile/features/common/buttons/solid/primary_text_button.dart';
+import 'package:hader_pharm_mobile/features/common/spacers/dividers.dart' show AppDivider;
+import 'package:hader_pharm_mobile/features/common/spacers/responsive_gap.dart';
 import 'package:hader_pharm_mobile/features/common/widgets/empty_list.dart';
 import 'package:hader_pharm_mobile/features/common_features/orders_details/actions/can_cancel_order.dart';
 import 'package:hader_pharm_mobile/features/common_features/orders_details/cubit/orders_details_cubit.dart';
 import 'package:hader_pharm_mobile/features/common_features/orders_details/widgets/cancel_order_bottom_sheet.dart';
 import 'package:hader_pharm_mobile/features/common_features/orders_details/widgets/order_client_note.dart';
+import 'package:hader_pharm_mobile/features/common_features/orders_details/widgets/order_infos_section.dart'
+    show OrderInfosSection;
 import 'package:hader_pharm_mobile/features/common_features/orders_details/widgets/order_invoice_section.dart';
 import 'package:hader_pharm_mobile/features/common_features/orders_details/widgets/order_summary_section.dart';
 import 'package:hader_pharm_mobile/features/common_features/orders_details/widgets/shipping_address_section.dart';
@@ -15,11 +19,11 @@ import 'package:hader_pharm_mobile/features/common_features/orders_details/widge
 import 'package:hader_pharm_mobile/utils/bottom_sheet_helper.dart';
 import 'package:hader_pharm_mobile/utils/enums.dart';
 import 'package:hader_pharm_mobile/utils/extensions/app_context_helper.dart';
+import 'package:hader_pharm_mobile/utils/extensions/app_date_helper.dart';
 
 class OrdersDetailsPage extends StatelessWidget {
   final String orderId;
-  static final GlobalKey<ScaffoldState> ordersDetailsScaffoldKey =
-      GlobalKey<ScaffoldState>();
+  static final GlobalKey<ScaffoldState> ordersDetailsScaffoldKey = GlobalKey<ScaffoldState>();
   const OrdersDetailsPage({super.key, required this.orderId});
   final bool isInvoiceWorkInProgress = true;
 
@@ -54,32 +58,57 @@ class OrdersDetailsPage extends StatelessWidget {
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
+                OrderInfosSection(
+                  orderRef: cubit.orderData!.displayId,
+                  createdAt: cubit.orderData!.createdAt.format,
+                ),
+                AppDivider(
+                  height: context.responsiveAppSizeTheme.current.p12,
+                  color: Colors.grey.shade100,
+                  endIndent: context.responsiveAppSizeTheme.current.s8,
+                  indent: context.responsiveAppSizeTheme.current.s8,
+                ),
                 ShippingAddressSection(
-                  address: item.deliveryAddress,
+                  address: cubit.orderData!.deliveryAddress,
                   latitude: cubit.orderData!.latitude,
                   longitude: cubit.orderData!.longitude,
                 ),
+                AppDivider(
+                  height: context.responsiveAppSizeTheme.current.p12,
+                  color: Colors.grey.shade100,
+                  endIndent: context.responsiveAppSizeTheme.current.s8,
+                  indent: context.responsiveAppSizeTheme.current.s8,
+                ),
                 Container(
-                  margin: EdgeInsets.symmetric(
-                      vertical: context.responsiveAppSizeTheme.current.p4),
-                  padding: EdgeInsets.symmetric(
-                      horizontal: context.responsiveAppSizeTheme.current.p4),
+                  margin: EdgeInsets.symmetric(vertical: context.responsiveAppSizeTheme.current.p4),
+                  padding: EdgeInsets.symmetric(horizontal: context.responsiveAppSizeTheme.current.p4),
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(context
-                        .responsiveAppSizeTheme.current.commonWidgetsRadius),
+                    borderRadius: BorderRadius.circular(context.responsiveAppSizeTheme.current.commonWidgetsRadius),
                   ),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       if (cubit.orderData!.invoiceType != null)
                         OrderInvoiceSection(
-                          invoiceType: InvoiceTypes.values.firstWhere(
-                              (element) =>
-                                  cubit.orderData!.invoiceType == element.id),
+                          invoiceType:
+                              InvoiceTypes.values.firstWhere((element) => cubit.orderData!.invoiceType == element.id),
                         ),
-                      if (cubit.orderData!.clientNote.isNotEmpty)
-                        ClientNoteSection(),
+                      if (cubit.orderData!.clientNote.isNotEmpty) ...[
+                        AppDivider(
+                          height: context.responsiveAppSizeTheme.current.p12,
+                          color: Colors.grey.shade100,
+                          endIndent: context.responsiveAppSizeTheme.current.s8,
+                          indent: context.responsiveAppSizeTheme.current.s8,
+                        ),
+                        ClientNoteSection()
+                      ],
+                      AppDivider(
+                        height: context.responsiveAppSizeTheme.current.p12,
+                        color: Colors.grey.shade100,
+                        endIndent: context.responsiveAppSizeTheme.current.s8,
+                        indent: context.responsiveAppSizeTheme.current.s8,
+                      ),
                       const OrderSummarySection(),
                       Padding(
                         padding: buttonsPadding,
@@ -87,8 +116,7 @@ class OrdersDetailsPage extends StatelessWidget {
                           label: translation.order_tracking,
                           onTap: () {
                             BottomSheetHelper.showCommonBottomSheet(
-                                context: context,
-                                child: OrderTrackingBottomSheet());
+                                context: context, child: OrderTrackingBottomSheet());
                           },
                           color: AppColors.accent1Shade1,
                         ),
@@ -98,14 +126,11 @@ class OrdersDetailsPage extends StatelessWidget {
                         child: PrimaryTextButton(
                           label: translation.item_complaint,
                           onTap: () {
-                            RoutingManager.router.pushNamed(
-                                RoutingManager.orderComplaint,
-                                extra: {
-                                  "orderId": item.id,
-                                  "itemId": item.id
-                                }).then((value) => {
-                                  if (value == true)
-                                    {cubit.getOrderComplaints()}
+                            RoutingManager.router.pushNamed(RoutingManager.orderComplaint, extra: {
+                              "orderId": item.id,
+                              "itemId": item.id
+                            }).then((value) => {
+                                  if (value == true) {cubit.getOrderComplaints()}
                                 });
                           },
                           color: AppColors.accent1Shade1,
@@ -117,9 +142,7 @@ class OrdersDetailsPage extends StatelessWidget {
                           child: PrimaryTextButton(
                             label: translation.invoice,
                             onTap: () {
-                              RoutingManager.router.pushNamed(
-                                  RoutingManager.invoiceScreen,
-                                  extra: orderId);
+                              RoutingManager.router.pushNamed(RoutingManager.invoiceScreen, extra: orderId);
                             },
                             color: AppColors.accent1Shade1,
                           ),
@@ -131,9 +154,7 @@ class OrdersDetailsPage extends StatelessWidget {
                             label: translation.cancel,
                             onTap: () {
                               BottomSheetHelper.showCommonBottomSheet(
-                                  initialChildSize: 0.3,
-                                  context: context,
-                                  child: CancelOrderBottomSheet());
+                                  initialChildSize: 0.3, context: context, child: CancelOrderBottomSheet());
                             },
                             color: theme.colorScheme.error,
                           ),
--- lib/features/common_features/orders_details/widgets/shipping_address_section.dart
+++ lib/features/common_features/orders_details/widgets/shipping_address_section.dart
@@ -6,38 +6,26 @@ class ShippingAddressSection extends StatelessWidget {
   final String address;
   final double latitude;
   final double longitude;
-  const ShippingAddressSection(
-      {super.key,
-      required this.address,
-      required this.latitude,
-      required this.longitude});
+  const ShippingAddressSection({super.key, required this.address, required this.latitude, required this.longitude});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(
-          vertical: context.responsiveAppSizeTheme.current.p12,
-          horizontal: context.responsiveAppSizeTheme.current.p4),
+          vertical: context.responsiveAppSizeTheme.current.p12, horizontal: context.responsiveAppSizeTheme.current.p4),
       width: double.maxFinite,
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(
-            context.responsiveAppSizeTheme.current.commonWidgetsRadius),
+        borderRadius: BorderRadius.circular(context.responsiveAppSizeTheme.current.commonWidgetsRadius),
       ),
       padding: EdgeInsets.symmetric(
-          vertical: context.responsiveAppSizeTheme.current.p6,
-          horizontal: context.responsiveAppSizeTheme.current.p8),
+          vertical: context.responsiveAppSizeTheme.current.p6, horizontal: context.responsiveAppSizeTheme.current.p8),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text(context.translation!.shipping_address,
-              style: context.responsiveTextTheme.current.headLine4SemiBold),
+          Text(context.translation!.shipping_address, style: context.responsiveTextTheme.current.headLine4SemiBold),
           const ResponsiveGap.s12(),
           Text(address, style: context.responsiveTextTheme.current.body3Medium),
-          const ResponsiveGap.s12(),
-          // Text(
-          //     "(Lat: ${latitude.toStringAsFixed(2)} , Long: ${longitude.toStringAsFixed(2)})",
-          //     style: context.responsiveTextTheme.current.body3Medium),
         ],
       ),
     );
