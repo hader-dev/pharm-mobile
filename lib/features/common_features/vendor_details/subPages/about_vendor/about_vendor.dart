c92fd76fd080401fc0b426cc4c62bca260282795
ayoub larbaoui <ayoublarbaoui004@gmail.com>
2025-11-23 00:04:33 -0800

    fix(vendor): restyled vendor card , restructured vendor details app bar & widgets.

--- lib/features/common_features/vendor_details/subPages/about_vendor/about_vendor.dart
+++ lib/features/common_features/vendor_details/subPages/about_vendor/about_vendor.dart
@@ -18,61 +18,22 @@ class VendorDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
         padding: EdgeInsets.symmetric(
-            vertical: context.responsiveAppSizeTheme.current.p24,
+            vertical: context.responsiveAppSizeTheme.current.p8,
             horizontal: context.responsiveAppSizeTheme.current.p16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: [
-            Row(children: [
-              Container(
-                height: context.responsiveAppSizeTheme.current.iconSize48,
-                width: context.responsiveAppSizeTheme.current.iconSize48,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: AppColors.bgDisabled, width: 1.5),
-                  image: DecorationImage(
-                    image: vendorData.image?.path == null
-                        ? AssetImage(DrawableAssetStrings.companyPlaceHolderImg)
-                        : NetworkImage(getItInstance
-                                .get<INetworkService>()
-                                .getFilesPath(vendorData.image?.path ?? ''))
-                            as ImageProvider,
-                  ),
-                ),
-              ),
-              const ResponsiveGap.s8(),
-              Text(vendorData.name,
-                  style: context.responsiveTextTheme.current.headLine4SemiBold
-                      .copyWith(
-                          color: AppColors.accent1Shade1,
-                          overflow: TextOverflow.ellipsis))
-            ]),
-            const ResponsiveGap.s12(),
-            Text(
-              context.translation!.description,
-              style: context.responsiveTextTheme.current.headLine3SemiBold
-                  .copyWith(color: AppColors.accent1Shade1),
-            ),
-            const ResponsiveGap.s12(),
-            Text(
-              vendorData.description ??
-                  context.translation!.no_description_available,
-              style: context.responsiveTextTheme.current.body2Regular,
-            ),
-            const ResponsiveGap.s12(),
             SpecificationsWidget(
               specifications: {
                 context.translation!.company_name: vendorData.name,
-                context.translation!.specialty: DistributorCategory.values
-                    .firstWhere((e) => e.id == vendorData.type)
-                    .name,
-                context.translation!.address: vendorData.address ??
-                    context.translation!.no_address_available,
-                context.translation!.phone:
-                    vendorData.phone ?? context.translation!.no_phone_available,
-                context.translation!.email:
-                    vendorData.email ?? context.translation!.no_email_available,
+                context.translation!.description:
+                    vendorData.description ?? context.translation!.no_description_available,
+                context.translation!.specialty:
+                    DistributorCategory.values.firstWhere((e) => e.id == vendorData.type).name,
+                context.translation!.address: vendorData.address ?? context.translation!.no_address_available,
+                context.translation!.phone: vendorData.phone ?? context.translation!.no_phone_available,
+                context.translation!.email: vendorData.email ?? context.translation!.no_email_available,
               },
             ),
           ],
