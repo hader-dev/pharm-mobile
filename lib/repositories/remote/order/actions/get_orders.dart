09c7803cce254ba8eca1f991e037619c45fa2f79
ayoub larbaoui <ayoublarbaoui004@gmail.com>
2025-12-16 00:35:07 -0800

    refactor (order repo) : edited order repo to get seller/client companies name field only .

--- lib/repositories/remote/order/actions/get_orders.dart
+++ lib/repositories/remote/order/actions/get_orders.dart
@@ -1,3 +1,5 @@
+import 'dart:convert' show jsonEncode;
+
 import 'package:flutter/material.dart';
 import 'package:hader_pharm_mobile/config/services/network/network_interface.dart';
 import 'package:hader_pharm_mobile/repositories/remote/order/params/get_orders.dart';
@@ -5,14 +7,14 @@ import 'package:hader_pharm_mobile/repositories/remote/order/response/order_resp
 import 'package:hader_pharm_mobile/utils/enums.dart';
 import 'package:hader_pharm_mobile/utils/urls.dart';
 
-Future<OrderResponse> getOrders(
-    ParamsGetOrder params, INetworkService client) async {
+Future<OrderResponse> getOrders(ParamsGetOrder params, INetworkService client) async {
   final Map<String, Object> queryParams = {
     'limit': params.limit.toString(),
     'offset': params.offset.toString(),
     'sort[id]': params.sortDirection,
-    if (params.searchQuery != null && params.searchQuery!.isNotEmpty)
-      'search[displayId]': params.searchQuery!,
+    "include[clientCompany][fields][]": ['name'],
+    "include[sellerCompany][fields][]": ['name'],
+    if (params.searchQuery != null && params.searchQuery!.isNotEmpty) 'search[displayId]': params.searchQuery!,
   };
 
   if (params.filters.status.isNotEmpty) {
